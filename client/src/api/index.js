c29136b4413afbf1ae9dd79e27b8772ee70255cb
Rudi Visagie <rudivisagie@icloud.com>
2022-11-07 14:28:39 +0200

    created api calls and implimented redux

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,1 +1,7 @@
 import axios from 'axios';
+
+//Define url that points to backend route.
+const url = "http://localhost:5000/posts"
+
+//Define function that retrieves posts from backend.
+export const fetchPosts = () => axios.get(url);
