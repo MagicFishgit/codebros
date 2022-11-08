d899857ce747fe32b14c1300ec25ef0d2712cee3
Rudi Visagie <rudivisagie@icloud.com>
2022-11-08 14:04:32 +0200

    card component displays correctly. some issue with storing data in db. probably due to the old deps. *sigh*

--- server/index.js
+++ server/index.js
@@ -8,14 +8,14 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
 // mongodb.com/cloud/atlas  with temporary credentials.
-const CONNECTION_URL = ;
+const CONNECTION_URL = 
 const PORT = process.env.PORT || 5000;
 
 // Connect to mongodb
